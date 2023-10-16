46088a546d398f9647d23851f81568ed25027829
Clément Chausson <clement.chausson@epitech.eu>
2023-10-16 14:12:34 +0200

    [+] Rewrite but it does not work...

--- next.config.js
+++ next.config.js
@@ -1,6 +1,26 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+//   basePath: process.env.BASE_PATH,
+//   assetPrefix: process.env.BASE_PATH,
 }
 
-module.exports = nextConfig
+let path = "/"
+
+if (process.env.BASE_PATH) {
+    path = process.env.BASE_PATH
+}
+
+module.exports = {
+    // basePath: process.env.BASE_PATH,
+    // assetPrefix: process.env.BASE_PATH,
+    async rewrites() {
+        return [
+            {
+                source: path,
+                destination: '/',
+            },
+        ]
+    },
+    nextConfig
+}
